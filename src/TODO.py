7bcef7fe130fd58c8c8652fde15e077b552fee06
matias francois <matiasf1997@hotmail.com>
2020-03-29 18:41:06 -0300

    actualice de nuevo el todo note

--- src/TODO.py
+++ src/TODO.py
@@ -16,9 +16,5 @@
 
 # Accesorios
 
-# que la ventana grafica aparezca mas alejada de la principal para no molestar
 # falta que se acoplen los tamaños al agrandar la pantalla
-# falta mejorar algunas cosas como visuales y ponerle titulo al menu principal
 # falta que se impida modificar los chechbox si se pone un muestreo (igual subjetivo)
-# si se toca de vuelta en el mismo boton la vuelve a graficar a la funcion no es nada grave pero mejor corregir
-# ¿falta tildar los checkbox? para impedir modificaciones si ya elegi el tipo de muestreo
